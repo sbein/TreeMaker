4a1789df71a55f0f75bb98c42a3cbcb1ddd8b0b4
your name your last name <samuel.bein@gmail.com>
2019-01-03 13:45:46 -0600

    dictionaries

--- Production/test/condorSub/dict_re2016F.py
+++ Production/test/condorSub/dict_re2016F.py
@@ -2,7 +2,7 @@ flist = {
     "scenario": "2016MiniAODv3",
     "samples": [
         #['Run2016F-17Jul2018-v1.HTMHT'],
-        #['Run2016F-17Jul2018-v1.JetHT'],
+        ['Run2016F-17Jul2018-v1.JetHT'],
         ['Run2016F-17Jul2018-v1.MET'],
         ['Run2016F-17Jul2018-v1.SingleElectron'],
         ['Run2016F-17Jul2018-v1.SingleMuon'],
--- Production/test/condorSub/dict_re2017F.py
+++ Production/test/condorSub/dict_re2017F.py
@@ -2,7 +2,7 @@ flist = {
     "scenario": "2017ReReco31Mar",
     "samples": [
         #['Run2017F-31Mar2018-v1.HTMHT'],
-        #['Run2017F-31Mar2018-v1.JetHT'],
+        ['Run2017F-31Mar2018-v1.JetHT'],
         ['Run2017F-31Mar2018-v1.MET'],
         ['Run2017F-31Mar2018-v1.SingleElectron'],
         #['Run2017F-31Mar2018-v1.SingleMuon'],
