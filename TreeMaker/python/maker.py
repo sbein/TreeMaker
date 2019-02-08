c7f6a65e9a426dedc4e4b1b30dcc33edc9a566ab
Viktor Kutzner <viktor.kutzner@cern.ch>
2019-02-08 18:26:04 +0100

    changed exit code to 0 if file exists

--- TreeMaker/python/maker.py
+++ TreeMaker/python/maker.py
@@ -61,8 +61,8 @@ def quitIfFileAlreadyProcessed(output_filename):
     status_viktor, output = commands.getstatusoutput("gfal-ls srm://dcache-se-cms.desy.de/pnfs/desy.de/cms/tier2/store/user/vkutzner/NtupleHub/" + output_filename + ".root")
     
     if status_sam == 0 or status_viktor == 0:
-        print "Output file already exists! Exiting"
-        quit(929292)
+        print "Output file already exists. That's ok, exiting"
+        quit(0)
     else:
         print "OK, proceeding"
 
